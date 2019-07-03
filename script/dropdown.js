9e8c08940bc9b4adbc56de4df98535b0dd6071fa
BenDuwe <benduwee@gmail.com>
2019-07-03 14:16:18 +0200

    sidebar v2.0

--- script/dropdown.js
+++ script/dropdown.js
@@ -1,6 +1,13 @@
 let drop_downs = document.querySelectorAll("li.dropdown");
 for (element of drop_downs) {
     element.children[0].addEventListener("click", function(){
+        for (elem of drop_downs) {
+            if (this.parentNode != elem && elem.classList.contains("active")) {
+                elem.classList.remove("active");
+            }
+        }
+
         this.parentNode.classList.toggle("active");
     });
 }
+
